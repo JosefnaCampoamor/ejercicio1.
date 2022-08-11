21afd636f84ebe75a6059186b680de320bd33e49
JosefnaCampoamor <110637414+JosefnaCampoamor@users.noreply.github.com>
2022-08-11 18:58:01 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+console.log("-cocinar una torta de haceite")
+console.log("paso uno: tomar una fuente")
+console.log("paso dos: agregar huevos, azúcar, leche y aceite")
+console.log("paso tres: tomar una batidora")
+console.log("paso cuatro: batir")
+console.log("paso cinco: tomar una taza")
+console.log("paso seis: agregar la harina al contenido")
+console.log("paso siete: batir todo")
+console.log("paso ocho: prender horno")
+console.log("paso nueve: tomar un molde")
+console.log("paso diez: enmantecar y enharinar el molde")
+console.log("paso once: berte el contenido")
+console.log("paso doce: ingrsar molde en el horno")
+console.log("esperar 45 minutos, para su cocción")
+
+console.log("-dirigirse hasta el supermercado")
+console.log("paso uno: preparar las bolasas del supermercado")
+console.log("paso dos: dirigirse al garage")
+console.log("paso tres: habrir portón")
+console.log("paso cuatro: dirigirme al auto")
+console.log("paso cinco: ingresar al auto")
+console.log("paso seis: hacer marcha atrás")
+console.log("paso siete: dirigirme hacia la calle")
+console.log("paso ocho: dirigirse hasta el supermercado")
+console.log("paso nueve: bajar del auto")
+console.log("paso diez: ingresar al supermercado")
+
+console.log("-crear un posteo en facebook")
+console.log("paso uno: encender el celular")
+console.log("paso dos: buscar la aplicación")
+console.log("paso tres: hacer click en el logo de faceboook")
+console.log("paso cuatro: en el muro")
+console.log("paso cinco: hacer clik en la barra abajo de publicaciones")
+console.log("paso seis: hacer click en el espacio de arriba")
+console.log("paso siete: redactar con el teclado")
+console.log("paso ocho: escribir texto")
+console.log("paso nueve: clickear el icono de imágen")
+console.log("paso diez: hacer click en una imgen")
+console.log("paso once: hacer click en publicar")
+
+console.log("-editar una foto")
+console.log("paso uno: encender celular")
+console.log("paso dos: buscar aplicación -galería-")
+console.log("paso tres: hacer click en una de las carpetas de camara")
+console.log("paso cuatro: hacer clikc en una foto")
+console.log("paso cinco: hacer click en en los tres botones superiores")
+console.log("paso seis: hacer click en editar")
+console.log("paso siete: hacer click en filtros")
+console.log("paso ocho: agregar filtro")
+console.log("paso nueve: acomo dar el tono del filtro")
+console.log("paso diez: guardar foto")
